b50614252f1e2d7ddfb31c67b7bfa79a149ffb66
Max Lutsenko <max.lutsenko@akelius.com>
2024-12-06 15:25:35 +0200

    findings

--- starwars/src/app/starship/starship.component.ts
+++ starwars/src/app/starship/starship.component.ts
@@ -1,3 +1,4 @@
+// TODO formatting and spacing
 import {Component,inject,OnInit} from '@angular/core';
 import {StarWarsService} from '../service/star-wars.service';
 import {ShipResult} from '../shared/models';
@@ -5,7 +6,6 @@ import {map} from 'rxjs';
 import {MatButton} from '@angular/material/button';
 import {Router, RouterLinkActive} from '@angular/router';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
-
 
 @Component({
   selector: 'app-starship',
@@ -18,43 +18,51 @@ import {MatProgressSpinner} from '@angular/material/progress-spinner';
   templateUrl: './starship.component.html',
   styleUrl: './starship.component.scss'
 })
-export class StarshipComponent implements OnInit{
+export class StarshipComponent implements OnInit {
   starWarsService = inject(StarWarsService);
   starships: ShipResult[] = [];
-  isApploading = false
+  // TODO use camelCase for properties
+  isAppLoading = false
 
-  constructor(private router: Router) {
+  constructor(private router: Router) {}
 
-  }
-
+  // TODO missing type void
   ngOnInit(){
-    this.isApploading=true
+    // TODO do not forget to unsubscribe and ; at the end of the line
+    // TODO missing unsubscription
+    this.isAppLoading = true;
     this.starWarsService.getShips().pipe(
       map((result)=>result.results),
     ).subscribe({
       next:(data)=>{
-        this.starships=data;
+        this.starships = data;
       },
       complete: () => {
-        this.isApploading = false
+        this.isAppLoading = false
       }
     })
-
   }
 
+  // TODO missing type void
   ShowDetails(id:string){
     this.router.navigate(['home/starships',id])
   }
 
+  // TODO missing type void
   LoadMore(){
+    // TODO missing unsubscription
+    // also we may improve it if we will use BehaviorSubject and store all data in one place
     this.starWarsService.getMoreStarShips().subscribe({
+      // TODO could be simplified just raw example
+      //   this.starWarsService.getMoreStarShips().pipe(
+      //   map(data => data.results),
+      //   map(results => this.starships.push(...results))
+      //   ).subscribe();
       next:(data)=>{
         for(let i=0;i<data.results.length;i++){
           this.starships.push(data.results[i])
         }
       }
     })
-
   }
-
 }
